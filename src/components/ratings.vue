a671324a254d3be5359432ff815e9dff0de7222a
Liiukenan <305290526@qq.com>
2018-01-19 10:31:29 +0800

    更新版本

---
+++ src/components/ratings.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    	<div>
+	   	{{$store.state.newslist}}
+	   </div>
+    </div>
+   
+</template>
+
+<script type="ecmascript-6">
+    
+</script>
+
+<style rel="stylesheet">
+    
+</style>
---
+++ src/components/seller.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>这是seller</div>
+</template>
+
+<script type="ecmascript-6">
+    
+</script>
+
+<style rel="stylesheet">
+    
+</style>
